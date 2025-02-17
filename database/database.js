f026ba1e20603ce96c5701e22601d8880bd3376b
ludovicspina <ludovic.spina@proton.me>
2025-02-18 00:46:55 +0100

    Update

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize('oracle', 'ludo', 'N9isrmb3@', {
-    host: '192.168.1.92',  // Votre Raspberry Pi
+    host: '192.168.1.116',  // Votre Raspberry Pi
     dialect: 'mysql',
     logging: false,  // Désactive les logs SQL
 });
--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const client = new Client({
 });
 
 // Syncro DB
-const { Salary, Subscription } = require('./database/models/finance');
 sequelize.sync().then(() => {
     console.log('Base de données synchronisée');
 }).catch(err => {
