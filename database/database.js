8af7d65dee852d9af5619650f951ce9cf3b4cc52
ludovicspina <ludovic.spina@proton.me>
2025-02-18 01:20:02 +0100

    Update

--- database/database.js
+++ database/database.js
@@ -1,7 +1,11 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('oracle', 'ludo', 'N9isrmb3@', {
+const { DATABASE_NAME } = process.env.DATABASE_NAME;
+const { DATABASE_PASSWORD } = process.env.DATABASE_PASSWORD;
+const { DATABASE_USER } = process.env.DATABASE_USER;
+
+const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, {
     host: '192.168.1.116',  // Votre Raspberry Pi
     dialect: 'mysql',
     logging: false,  // Désactive les logs SQL
